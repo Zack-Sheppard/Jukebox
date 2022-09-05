ffa09f961da871411b4116e3751c59c6889efa11
Zack Sheppard <zack.sheppard@gmail.com>
2022-09-05 11:36:09 -0600

    reconfigure ts settings on server side

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,14 +1,40 @@
-import express from "express";
-const app = express();
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-import * as dotenv from "dotenv";
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const path_1 = __importDefault(require("path"));
+const dotenv = __importStar(require("dotenv"));
 dotenv.config();
 const host = process.env.host;
 const port = process.env.port;
-app.use(express.static(path.join(__dirname, "../../webapp/public")));
-app.set("views", path.join(__dirname, "../../webapp/views"));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../webapp/public")));
+app.set("views", path_1.default.join(__dirname, "../../webapp/views"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("home");
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,8 @@
 
-import express from "express";
-const app: express.Application = express();
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
 
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import path from "path";
 
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -16,7 +14,7 @@ app.use(express.static(path.join(__dirname, "../../webapp/public")));
 app.set("views", path.join(__dirname, "../../webapp/views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/", (req: Request, res: Response) => {
     res.render("home");
 });
 
