b61fc08c3eeb724ff4c4a58da20d04a7ca4427af
Zack Sheppard <zack.sheppard@gmail.com>
2023-04-16 19:37:14 -0600

    host room prototype

--- webapp/src/host.tsx
+++ webapp/src/host.tsx
@@ -5,50 +5,196 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {} from "react/next";
 
+import Header from "./components/header";
 import Button from "./components/button";
+import Form from "./components/form";
 
-class Host extends React.Component<{}, {}> {
+const url: string = window.location.href;
+
+// perhaps better is find the "room="
+const room_number: string = url.slice(-3);
+
+class SpotifyTrack {
+  trackName: string;
+  artistName: string;
+  imageUrl: string;
+  spotifyID: string;
+
+  constructor(trackName: string,
+              artistName: string,
+              imageUrl: string,
+              spotifyID: string) {
+    this.trackName = trackName;
+    this.artistName = artistName;
+    this.imageUrl = imageUrl;
+    this.spotifyID = spotifyID;
+  }
+}
+
+function SongRow(props: any) {
+  return(
+    <tr onClick={props.onClick}>
+      <td>
+        <img src={props.image} alt="img" width="64" height="64"/>
+      </td>
+      <td>
+        <p className="artist-name">{props.artistName}</p>
+        <p className="song-name">{props.trackName}</p>
+      </td>
+    </tr>
+  );
+}
+
+interface SearchResultProps {
+  isSearching: boolean,
+  results: any,
+  onClick: ((s: SpotifyTrack) => any)
+}
+
+class SearchResult extends React.Component<SearchResultProps> {
+  constructor(props: SearchResultProps) {
+    super(props);
+  }
+
+  renderRow(i: number) {
+    let t = this.props.results[i];
+    let track = new SpotifyTrack(t.trackName,
+                                 t.artistName,
+                                 t.imageUrl,
+                                 t.spotifyID);
+    return(
+      <SongRow
+        key={i.toString()}
+        onClick={() => this.props.onClick(track)}
+        spotifyID={t.spotifyID}
+        image={t.imageUrl}
+        trackName={t.trackName}
+        artistName={t.artistName}
+      />
+    );
+  }
+
+  render() {
+    if(this.props.isSearching) {
+      console.log("Trying to render N rows:");
+      console.log(this.props.results.length);
+      let rows = [];
+      for(let i = 0; i < this.props.results.length; i++) {
+        rows.push(this.renderRow(i));
+      }
+      if(this.props.results.length == 0) {
+        return(
+          <p id="search-song-result">
+            Didn't find any songs!
+          </p>
+        );
+      }
+      else {
+        return(
+          <table id="search-song-result">
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
+        );
+      }
+    }
+    return null;
+  }
+}
+
+class Host extends React.Component<{},
+                                   {searching: boolean,
+                                    search: string,
+                                    searchResults: any[]}> {
   constructor(props: any) {
     super(props);
-    this.editRoom = this.editRoom.bind(this);
-    this.getDevices = this.getDevices.bind(this);
-    this.getQueue = this.getQueue.bind(this);
+    this.state = {
+      searching: false,
+      search: "",
+      searchResults: [],
+    };
+    this.handleSearchRes = this.handleSearchRes.bind(this);
+    this.addToQueue = this.addToQueue.bind(this);
   }
 
-  editRoom() {
-    console.log("room button clicked");
-    // no op for a while
+  copyInviteLink() {
+    console.log("Copying URL to clipboard!");
+    navigator.clipboard.writeText(url);
+    // maybe lil success message
   }
 
-  getDevices() {
-    // send GET to backend
-    console.log("sending fetch!");
+  handleSearchRes(res: any) {
+    let resultArray = res.result;
+    console.log("number of incoming results:");
+    console.log(resultArray.length);
+    let spotifyTracks = new Array(resultArray.length);
+
+    for(let i = 0; i < resultArray.length; i++) {
+      // song title, artist name, image url, Spotify ID
+      spotifyTracks[i] = new SpotifyTrack(
+        resultArray[i].name,
+        resultArray[i].artists[0].name,
+        resultArray[i].album.images[0].url,
+        resultArray[i].id
+      );
+    }
+    console.log("displaying search results:");
+    this.setState(() => ({
+      searching: true,
+      search: "",
+      searchResults: spotifyTracks,
+    }));
   }
 
-  getQueue() {
-    console.log("getting queue!");
+  addToQueue(track: SpotifyTrack) {
+    this.setState(() => ({
+      searching: false
+    }));
+    console.log("adding to queue: " + track.trackName);
+    fetch(`/room/${room_number}/add?songID=${track.spotifyID}`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error("bad network response");
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log("server responded with:");
+      console.log(data);
+    })
+    .catch(error => {
+      console.log("there was an error:", error);
+    });
   }
 
   render() {
     return (
-    <div id="host">
-      <Button
-        enabled={false}
-        onClick={this.editRoom}
-        text="Room 123"
-        type="button"
+    <div id="room">
+      <Header
+        host={true}
       />
-      <Button
-        enabled={true}
-        onClick={this.getDevices}
-        text="Get Devices"
-        type="button"
+      <div id="room-num">
+        <p>Room</p>
+        <Button
+          enabled={true}
+          host={true}
+          onClick={this.copyInviteLink}
+          text={room_number}
+          type="button"
+        />
+      </div>
+      <Form
+        name="song"
+        maxLength={48}
+        placeholder="Search for a song"
+        onResponse={this.handleSearchRes}
+        uri="/spotify/search"
       />
-      <Button
-        enabled={true}
-        onClick={this.getQueue}
-        text="Get Queue"
-        type="button"
+      <SearchResult
+        onClick={spotifyTrack => this.addToQueue(spotifyTrack)}
+        isSearching={this.state.searching}
+        results={this.state.searchResults}
       />
     </div>
     );
