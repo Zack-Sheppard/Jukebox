c47c72a2acc6148d35cd9de671aae3a2fef72dbd
ChrisSyrnyk <christopher.syrnyk2@ucalgary.ca>
2023-04-16 18:56:59 -0600

    Added if statement for display

--- webapp/src/components/modal.tsx
+++ webapp/src/components/modal.tsx
@@ -12,14 +12,16 @@ export default class Modal extends React.Component<ModalProps>{
 
 
     render(){
-        return(
-            <div className='modal-container'>
-                <div className='modal'>
-                <div className='modal-exit'> {/*Add on click to close*/}
-                    <div className="center-text">X</div>
+        if(this.props.display === true){
+            return(
+                <div className='modal-container'>
+                    <div className='modal'>
+                    <div className='modal-exit'> {/*Add on click to close*/}
+                        <div className="center-text">X</div>
+                    </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-        )
+            )
+        }
     }
 }
